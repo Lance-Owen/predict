b66a107b6bb26621e0a372ac6c2da27d5492f715
Lance <75961930+Lance-Owen@users.noreply.github.com>
2023-08-11 18:00:49 +0800

    Update predict_chizhou3.py

--- predict_chizhou3.py
+++ predict_chizhou3.py
@@ -17,8 +17,8 @@ df = df[[
     'zbkzj', 'kbjj', 'k1', 'k2', '中标金额', '下浮率', '投标人数', 'project_type', '取值范围', '年', '月', '日', '时', '分']]
 
 
-degree = 1
-pre_label = 'k2'
+degree = 4
+pre_label = '下浮率'
 df1 = df[df['取值范围'].str.contains('0.5-10')]
 df1 = df1[['zbkzj','年', '月', '日', '时', '分', pre_label]]
 df1 = df1.fillna(-999)
@@ -27,17 +27,23 @@ df1 = df1.fillna(-999)
 
 
 
-df2 = df[df['取值范围'].str.contains('0.5-15')]
+# df2 = df[df['取值范围'].str.contains('0.5-15')]
+df2 = df[df['取值范围']=='0.5-15']
 df2 = df2[['zbkzj','年', '月', '日', '时', '分', pre_label]]
 df2 = df2.fillna(-999)
-# print(record_predict(df2,[16216306.33,2023,8,18,8,30],degree))
-print(record_predict(df2,[9673409.49,2022,8,25,9,0],degree))
+print(record_predict(df2,[8605491.13,2023,8,10,8,30],4))
+print(record_predict(df2,[8605491.13,2023,8,10,8,30],3))
+print(record_predict(df2,[8605491.13,2023,8,10,8,30],2))
+print(record_predict(df2,[8605491.13,2023,8,10,8,30],1))
+
+# print(record_predict(df2,[9673409.49,2022,8,25,9,0],degree))
 
 
 df3 = df[df['取值范围'].str.contains('98-102')]
+# df3 = df[df['取值范围']=='98-102']
 df3 = df3[['zbkzj','年', '月', '日', '时', '分', pre_label]]
 df3 = df3.fillna(-999)
-# print(record_predict(df3,[18361714.36,2023,8,2,9,0],degree))
+# print(record_predict(df3,[3606839.92,2023,8,24,10,0],degree))
 
 
 df4 = df[df['取值范围'].str.contains('k:0.1，0.2，0.3')]
